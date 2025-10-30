c4f2c52f51ef1c5a5ca7fc0d2163fb60adacb465
ST10439676 <ST10439676@vcconnect.edu.za>
2025-10-30 17:11:12 +0200

    Get Access to the DisplayAttribute name of Role enum

--- MvcIdentiyFirstPrinciples/Constants.cs
+++ MvcIdentiyFirstPrinciples/Constants.cs
@@ -1,5 +1,7 @@
 using System;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.Reflection;
 
 namespace MvcIdentiyFirstPrinciples;
 
@@ -9,4 +11,13 @@ public enum Roles
     ADMIN_ROLE,
     [Display(Name = "User")]
     USER_ROLE
+}
+
+public static class RoleDisplayName {
+    public static  string GetDisplayName(Roles role)
+    {
+        FieldInfo info = typeof(Roles).GetField(Enum.GetName(role)!);
+        var display = info.GetCustomAttribute<DisplayAttribute>();
+        return display.GetName();
+    }
 }
